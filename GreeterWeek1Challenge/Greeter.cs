165e62cf010e236521672525d192fac5194b7fd1
codyamorton <codyamorton2020@gmail.com>
2020-11-23 19:26:50 -0500

    updated submission for week 2 challenge

---
+++ GreeterWeek1Challenge/Greeter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterWeek1Challenge
+{
+    class Greeter
+    {
+        public void HelloThere(string name)
+        {
+            Console.WriteLine("Hello there, " + name);
+        }
+
+        public void ByeBye(string name)
+        {
+            Console.WriteLine("See ya," + name + ".");
+        }
+
+        public void TimeBased(string name)
+        {
+            if (DateTime.Now.Hour >= 6 && DateTime.Now.Hour < 12)
+
+            {
+                Console.WriteLine("Good Morning" + name + ".");
+            }
+
+            else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 16)
+            {
+                Console.WriteLine("Good Afternoon" + name + ".");
+            }
+
+            else if (DateTime.Now.Hour >= 16 && DateTime.Now.Hour < 20)
+            {
+                Console.WriteLine("Good Evening" + name + ".");
+            }
+
+            else if (DateTime.Now.Hour >= 20 && DateTime.Now.Hour < 24)
+            {
+                Console.WriteLine("Good Night" + name + ".");
+            }
+        }
+    }
+}
---
+++ GreeterWeek1Challenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterWeek1Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Greeter userInput = new Greeter();
+
+            userInput.HelloThere(name);
+            userInput.ByeBye(name);
+            userInput.TimeBased(name);
+            Console.ReadLine();
+
+        }
+    }
+}
+
